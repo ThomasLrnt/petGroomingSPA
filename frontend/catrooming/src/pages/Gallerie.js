546b39cff4b2aca69236e6db08767f711a8fb8f8
ThomasLrnt <laurent.thomas90@gmail.com>
2020-02-09 06:13:57 +0100

    Création de la gallerie de photos avec l'API ; installation du composant Gallery.

--- frontend/catrooming/src/pages/Gallerie.js
+++ frontend/catrooming/src/pages/Gallerie.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Kitty from '../fonctions/kitty';
 
 class Gallerie extends React.Component {
     render() {
         return (
             <div class="container">
                 <h1>Gallerie</h1>
-                <h3>HELLO WORLD !!</h3>
+                <Kitty />
             </div>
         );
     }
